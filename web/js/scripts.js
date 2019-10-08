95a84da28cbb7841a4ab199986a005d24190b184
Adam <amoliski@gmail.com>
2019-10-08 18:48:55 -0500

    Style tweaks, scroll to top button on articles

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -7,6 +7,24 @@ function ready(fn) {
     }
 }
 
+// c = element to scroll to or top position in pixels
+// e = duration of the scroll in ms, time scrolling
+// d = (optative) ease function. Default easeOutCuaic
+function scrollTo(c,e,d){d||(d=easeOutCuaic);var a=document.documentElement;
+    if(0===a.scrollTop){var b=a.scrollTop;++a.scrollTop;a=b+1===a.scrollTop--?a:document.body}
+    b=a.scrollTop;0>=e||("object"===typeof b&&(b=b.offsetTop),
+    "object"===typeof c&&(c=c.offsetTop),function(a,b,c,f,d,e,h){
+        function g(){0>f||1<f||0>=d?a.scrollTop=c:(a.scrollTop=b-(b-c)*h(f),
+            f+=d*e,setTimeout(g,e))}g()}(a,b,c,0,1/e,20,d))};
+function easeOutCuaic(t){t--;return t*t*t+1;}
+
+function scroll_to_top(e){
+    e.preventDefault();
+    let height = document.body.scrollHeight;
+    scrollTo(0, height/8);
+    return false;
+}
+
 (function(){
     let set_up_ga = function(){
         window.dataLayer = window.dataLayer || [];
